6f61865c2f4c41f28f9dfeb1137b2eaca206cb3e
Joshiy13 <joshua@weidanz.de>
2024-02-19 20:56:56 +0100

    Remove unnecessary files and code & better structure

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,9 +1,8 @@
 browser.browserAction.onClicked.addListener((tab) => {
-  // Open the settings window when the toolbar icon is clicked.
-  browser.tabs.create({ url: "scripts/settings.html" });
+  browser.tabs.create({ url: "html/settings.html" });
 });
 
-// Add listener for pinning the extension to the toolbar.
+
 browser.runtime.onInstalled.addListener(() => {
   browser.browserAction.setBadgeText({ text: "" }); // Clear badge text if any.
   browser.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] }); // Clear badge color if any.
--- src/scripts/distance_converter.js
+++ src/scripts/distance_converter.js
@@ -1,20 +1,20 @@
 browser.storage.local.get(["km", "mile", "feet", "meter"]).then((result) => {
     const { km, mile, feet, meter } = result;
 
-    // Apply conversions based on user preferences
+
     if (km) {
-        // Apply kilometers conversion
+
     }
 
     if (mile) {
-        // Apply miles conversion
+
     }
 
     if (feet) {
-        // Apply feet conversion
+ 
     }
 
     if (meter) {
-        // Apply meters conversion
+
     }
 });
